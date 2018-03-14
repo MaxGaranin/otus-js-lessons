73ce126de923c4873f8ef924970fffa057862998
GaraninMS <GaraninMS@samnipineft.ru>
2018-03-14 11:56:11 +0400

    Первая версия, пока без параметра пути к папке

--- lesson04/tree.js
+++ lesson04/tree.js
@@ -1,14 +1,48 @@
-console.log('Start tree');
-
 const fs = require('fs');
-var path = require('path')
+const path = require('path')
 
-fs.readdir('./node_modules/jquery', function (err, files) {
+const inputDir = './node_modules';
+
+readFiles(inputDir, printResult);
+
+function readFiles(dir, callback) {
+  var result = {
+    files: [],
+    folders: []
+  };
+
+  readFilesEx(dir, result);
+
+  setTimeout(function () {
+    callback(result);
+  }, 1000);
+}
+
+function readFilesEx(dir, result) {
+  fs.readdir(dir, function (err, files) {
     if (err) return console.error(err);
 
-    files.forEach(function(file) {
-        //if (path.extname(file) === ext) {
-          console.log(file);
-        //}
+    files.forEach(function (file) {
+      var fileName = path.join(dir, file);
+      if (fs.lstatSync(fileName).isDirectory()) {
+        result.folders.push(file);
+        readFilesEx(fileName, result);
+      }
+      else {
+        result.files.push(file);
+      }
     });
   });
+}
+
+function printResult(result) {
+  console.log(`\nFolders: ${result.folders.length}`);
+  result.folders.forEach(function (file) {
+    console.log(file);
+  });
+
+  console.log(`\nFiles: ${result.files.length}`);
+  result.files.forEach(function (file) {
+    console.log(file);
+  });
+}
