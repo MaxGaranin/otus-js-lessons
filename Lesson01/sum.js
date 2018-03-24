8715f1a0163ef807437ab6975dc44f391865f919
GaraninMS <maxgaranin@gmail.com>
2018-03-24 23:30:49 +0400

    Использование arguments для определения количества входных аргументов

--- Lesson01/sum.js
+++ Lesson01/sum.js
@@ -1,14 +1,19 @@
 function sum(a) {
-  if (a === undefined) throw new SyntaxError("Can't sum undefined values.");
-  
-  // Сначала хотелось сделать так, но в этом случае возникают проблемы с вызовом sum()
-  // if (a === undefined) a = 0;  
+  if (arguments.length == 0)
+    throw new SyntaxError("Without arguments must be only last brackets.");
+
+  if (a === undefined)
+    throw new SyntaxError("Can't sum undefined values.");
 
   var currentSum = a;
 
   function f(b) {
-    if (b === undefined) return currentSum;
-    
+    if (arguments.length == 0)
+      return currentSum;
+
+    if (b === undefined)
+      throw new SyntaxError("Can't sum undefined values.");
+
     currentSum += b;
     return f;
   }
@@ -16,8 +21,8 @@ function sum(a) {
   return f;
 }
 
-console.log( sum(1)(2)() );
-console.log( sum(3)(-1)(2)() );
-console.log( sum(7)() );
-console.log( sum() );
-console.log( sum(undefined)(5)() );
+console.log(sum(1)(2)());
+console.log(sum(3)(-1)(2)());
+console.log(sum(7)());
+console.log(sum());
+console.log(sum()(5)());
