74f07856b360484b41acfffb1ca3f6372aba545a
Max Garanin <m_garanin@mail.ru>
2018-04-10 15:58:14 +0400

    Рефакторинг примеров

--- lesson09_11/weather-app/src/MenuExample.js
+++ lesson09_11/weather-app/src/MenuExample.js
@@ -1,55 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class MenuExample extends Component {
     constructor() {
         super();
+        this.state = {
+            focused: 0
+        }
     }
-
-    componentWillMount() {
-        this.setState({ focused: 0 });
-    }
-
-    clicked(index) {
-
-        // Обработчик клика обновит состояние
-        // изменив индекс на сфокусированный элемент меню
-
-        this.setState({ focused: index });
-    };
 
     render() {
-
-        // Здесь мы читаем свойство items, которое было передано
-        // атрибутом, при создании компонента
-
         var self = this;
-
-        // Метод map пройдется по массиву элементов меню,
-        // и возвратит массив с <li> элементами.
 
         return (
             <div>
                 <ul>{this.props.items.map(function (m, index) {
-
                     var style = '';
-
-                    if (self.state.focused == index) {
+                    if (self.state.focused === index) {
                         style = 'focused';
                     }
 
-                    // Обратите внимание на использование метода bind(). Он делает
-                    // index доступным в функции clicked:
-
-                    return <li className={style} onClick={self.clicked.bind(self, index)}>{m}</li>;
-
+                    return (
+                        <li className={style} onClick={self.clicked.bind(self, index)}>
+                            {m}
+                        </li>
+                    );
                 })}
-
                 </ul>
 
                 <p>Selected: {this.props.items[this.state.focused]}</p>
             </div>
         );
+    }
 
+    clicked(index) {
+        this.setState({focused: index});
     }
 }
 
