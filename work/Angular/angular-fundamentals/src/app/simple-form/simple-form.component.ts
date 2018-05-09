04a2a775c7a7fd8b04e1b9d737eb03f39d0f325a
GaraninMS <maxgaranin@gmail.com>
2018-05-09 23:23:40 +0400

    Разбор туториала Getting Started With Angular https://egghead.io/courses/get-started-with-angular

---
+++ work/Angular/angular-fundamentals/src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-form',
+  template: `
+    <div>
+      <input #myInput type="text"/>
+      <button (mouseover)="onClick($event, myInput.value)">Click me!</button>
+    </div>
+  `,
+  styles: []
+})
+
+export class SimpleFormComponent implements OnInit {
+
+  onClick(event, value) {
+    console.log(event);
+    console.log(value);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
