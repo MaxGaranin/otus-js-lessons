c6cacc38db0eea5883f4fda380803395e25a8a20
GaraninMS <maxgaranin@gmail.com>
2018-06-20 22:30:36 +0400

    Убрал файлы, не относящиеся к заданию

---
+++ lesson07/async-series-console.js
@@ -0,0 +1,23 @@
+const seriesAsync = require('./series').seriesAsync;
+
+var fn1 = function (next) {
+    console.log('Function 1...');
+    next();
+}
+
+var fn2 = function (next) {
+    console.log('Function 2...');
+    next("hello world");
+}
+
+var fn3 = function (next) {
+    console.log('Function 3...');
+    next();
+}
+
+var fn4 = function (next) {
+    console.log('Last function...');
+    next();
+}
+
+seriesAsync(fn1, fn2, fn3, fn4);
